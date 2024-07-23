791fb0fb86e180797d42c6f0be5e1edcf5bed2b4
Elijah Logan <elijah.logan@purchase.edu.com>
2024-07-23 12:54:24 -0400

    h1 emulating kjeyframes

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ const fade  = useSpring({
 
 });
 
+let {x} = useSpring({
+  x: isToggled ? 0:100
+ });
+
 let navAnimate = useSpring({
  transform: isNav ? `translate3d(0, 0,0)`:`translate3d(100%, 0,0)`
 })
@@ -26,14 +30,23 @@ console.log(fade)
   return (
     <animated.div className="App" style={fade}>
       <header className="App-header">
+    {/* // emulating keyframes */}
+      <animated.h1 style={{
+        transform: x.interpolate({
+          range:[0,.25,.50,.86,100], 
+          output:[0, 50, 75,85,100]}).interpolate(
+          x => `translate3d(${x}%,0,0)`)
+      }}>hello world </animated.h1>
+
+
         <img src={logo} className="logo" />
         <button className="menu-button" onClick = {() => setNav(!isNav)}>change nav</button>
-
+   
       {/* <Nav style={navAnimate}/> */}
       </header>
       <main>
-        
-        <Toggle/>
+        <button onClick = {()=> {setToggle(!isToggled)}}> hello button</button>
+        <Toggle />
         <Checkout isOpen={isNav}/>
       </main>
     </animated.div>

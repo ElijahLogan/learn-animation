85669f75c19c07e52c4629ad425147fe880b3259
Elijah Logan <elijah.logan@purchase.edu.com>
2024-07-23 13:20:48 -0400

    basic mounting set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {  useState, Component } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, useTransition} from 'react-spring';
 import logo from './logo.svg';
 import './App.css';
 import Toggle from './toggle';
@@ -7,49 +7,19 @@ import Nav from './nav';
 import Checkout from './checkout';
 const App = () => {
   const [isToggled, setToggle] = useState(false)
-  const [isNav, setNav] = useState(false)
-// create object of animation'
-const fade  = useSpring({
-  from:{
-    opacity:0
-  },
-
-    opacity:1
-
-});
-
-let {x} = useSpring({
-  x: isToggled ? 0:100
- });
-
-let navAnimate = useSpring({
- transform: isNav ? `translate3d(0, 0,0)`:`translate3d(100%, 0,0)`
-})
-console.log(fade)
+  const transition = useTransition(isToggled,null,{
+    from: {opacity:0},
+    enter: {opacity:1},
+    leave: {opacity:0}
+  }
+  )
 
   return (
-    <animated.div className="App" style={fade}>
-      <header className="App-header">
-    {/* // emulating keyframes */}
-      <animated.h1 style={{
-        transform: x.interpolate({
-          range:[0,.25,.50,.86,100], 
-          output:[0, 50, 75,85,100]}).interpolate(
-          x => `translate3d(${x}%,0,0)`)
-      }}>hello world </animated.h1>
-
-
-        <img src={logo} className="logo" />
-        <button className="menu-button" onClick = {() => setNav(!isNav)}>change nav</button>
-   
-      {/* <Nav style={navAnimate}/> */}
-      </header>
-      <main>
-        <button onClick = {()=> {setToggle(!isToggled)}}> hello button</button>
-        <Toggle />
-        <Checkout isOpen={isNav}/>
-      </main>
-    </animated.div>
+    <div>
+      {transition.map(({item, key,props}) => (item && <animated.h1 key ={key} style={props}> Hello</animated.h1>))}
+      <animated.h1> Hello</animated.h1>
+      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+    </div>
   );
 }
 

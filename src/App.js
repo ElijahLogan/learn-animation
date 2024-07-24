c33ce1c07ee95aa0a1b1338599f08aed9e098587
Elijah Logan <elijah.logan@purchase.edu.com>
2024-07-24 14:25:45 -0400

    switch between components

--- src/App.js
+++ src/App.js
@@ -6,32 +6,30 @@ import Toggle from './toggle';
 import Nav from './nav';
 import Checkout from './checkout';
 const App = () => {
-  const [isToggled, setToggle] = useState(false)
+  const [items, setItems] = useState([{letter:'6',key:1},{letter:'7',key:2}, {letter:'8',key:3}])
   // params: boolena, keys, object: from/enter/leave
-  const transition = useTransition(isToggled,null,{
+  const transition = useTransition(items,item => item.key,{
     // position absolute: relative container needed to manage it
-    from: {opacity:0,  position: 'absolute'},
+    from: {opacity:0},
     enter: {opacity:1},
     leave: {opacity:0}
   }
   )
 
   return (
-    <div>
+    <div >
 
       {transition.map(
             ({item, key,props}) => 
-            (item ?
-             (<animated.h1 key ={key} style={props}> 
-              Hello World</animated.h1>) 
-              : 
-              ( <animated.h1 key ={key} style={props}> 
-                Bye World </animated.h1>)
+           
+             <animated.h1 key ={key} style={props}>
+                {items.letter}
+              </animated.h1>
             
-            ))}
+            )}
 
 
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      <button onClick={() => setItems({"letter":6, key:1})}>Toggle</button>
     </div>
   );
 }
@@ -40,3 +38,42 @@ const App = () => {
 
 
 export default App;
+
+
+
+
+// const App = () => {
+//   const [isToggled, setToggle] = useState(false)
+//   // params: boolena, keys, object: from/enter/leave
+//   const transition = useTransition(isToggled,null,{
+//     // position absolute: relative container needed to manage it
+//     from: {opacity:0,  position: 'absolute'},
+//     enter: {opacity:1},
+//     leave: {opacity:0}
+//   }
+//   )
+
+//   return (
+//     <div style = {{position:'relative'}}>
+
+//       {transition.map(
+//             ({item, key,props}) => 
+//             (item ?
+//              (<animated.h1 key ={key} style={props}> 
+//               Hello World</animated.h1>) 
+//               : 
+//               ( <animated.h1 key ={key} style={props}> 
+//                 Bye World </animated.h1>)
+            
+//             ))}
+
+
+//       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+//     </div>
+//   );
+// }
+
+
+
+
+// export default App;
